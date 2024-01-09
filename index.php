35283973d1b795a738e1045c26bdbc18b1dfa313
Piotr Walesiuk <49117833+pwalesiuk@users.noreply.github.com>
2024-01-09 09:47:16 +0100

    dodanie pokazywanie uptime

--- index.php
+++ index.php
@@ -2,12 +2,22 @@
 
 require('inc/wol_lib.php');
 
+function get_upt() {
+  $str   = @file_get_contents('/proc/uptime');
+  $num   = intval($str);
+  $secs  = fmod($num, 60); $num = intdiv($num, 60);
+  $mins  = $num % 60;      $num = intdiv($num, 60);
+  $hours = $num % 24;      $num = intdiv($num, 24);
+  $days  = $num;
+  return sprintf('%d days, %02d:%02d:%02d', $days, $hours, $mins, $secs);
+}
+
 //$wol_lib->scan();
 $hosts = $wol_lib->get_hosts();
 $harp = $wol_lib->get_harp();
 
 
-$smarty->assign('sesja', wol_lib::version . ' (' . PHP_VERSION . ')');
+$smarty->assign('sesja', wol_lib::version . ' (' . PHP_VERSION . ')' . ' [uptime: ' . get_upt() . ']');
 $smarty->assign('hosts', $hosts);
 $smarty->assign('harp', $harp);
 $smarty->assign('logs', $wol_lib->get_logs());
